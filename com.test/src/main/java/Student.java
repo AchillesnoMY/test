2de3800929102e26e0a67996aa3cb487c83e107b
君召 <mengyang.cui@alibaba-inc.com>
2022-10-13 14:45:24 +0800

    测试

---
+++ com.test/src/main/java/Student.java
@@ -0,0 +1,12 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+
+/**
+ * @author 君召
+ * @version $Id: Student.java, v 0.1 2022-10-13 2:39 PM
+ */
+public class Student {
+    private String name;
+}
